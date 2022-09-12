37c8751d10c346f532bd0cdb37ab1cb549bea258
piotrciarka <piotrjciarka@gmail.com>
2022-09-12 13:42:20 +0200

    randomize weather data

---
+++ index.ts
@@ -0,0 +1,33 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+const WEATHER_ROUTE = new URLPattern({ pathname: "/weather/:city" });
+
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomizedWeatherData() {
+  return [
+    { city: "Cracow", temperature: getRandomArbitrary(18, 22) },
+    { city: "Athens", temperature: getRandomArbitrary(27, 33) },
+    { city: "Rome", temperature: getRandomArbitrary(24, 29) },
+    { city: "Sydney", temperature: getRandomArbitrary(22, 28) },
+  ]
+}
+
+function handler(req: Request): Response {
+  const match = WEATHER_ROUTE.exec(req.url);
+  if (match) {
+    const weatherData = getRandomizedWeatherData();
+    const cityName = match.pathname.groups.city;
+    const cityWeatherData = weatherData.find((data) => data.city === cityName);
+    if (cityWeatherData) {
+      return Response.json({ temperature: cityWeatherData.temperature });
+    }
+  }
+  return new Response("City not found", {
+    status: 404,
+  });
+}
+
+serve(handler);
