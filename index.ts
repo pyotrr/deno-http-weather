78dd27fe11000e3aa1c18f046d604bb672ed4e3f
piotrciarka <piotrjciarka@gmail.com>
2022-09-12 14:10:21 +0200

    cors and content type

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ function getRandomizedWeatherData() {
     { city: "Athens", temperature: getRandomArbitrary(27, 33) },
     { city: "Rome", temperature: getRandomArbitrary(24, 29) },
     { city: "Sydney", temperature: getRandomArbitrary(22, 28) },
-  ]
+  ];
 }
 
 function handler(req: Request): Response {
@@ -22,11 +22,20 @@ function handler(req: Request): Response {
     const cityName = match.pathname.groups.city;
     const cityWeatherData = weatherData.find((data) => data.city === cityName);
     if (cityWeatherData) {
-      return Response.json({ temperature: cityWeatherData.temperature });
+      return Response.json({ temperature: cityWeatherData.temperature }, {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
     }
   }
   return new Response("City not found", {
     status: 404,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
 
